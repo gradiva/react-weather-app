cfe30e1d94a3bd035ce14f9220f9f671e374c8ca
Leticia <leticia.campos.cm@gmail.com>
2019-09-01 19:43:49 +0200

    Extract api logic and add statuses

---
+++ src/api.js
@@ -0,0 +1,18 @@
+const endpoint = 'http://api.openweathermap.org/data/2.5/weather';
+const appId = 'aff2b592970a65abe4953650a6a956d7';
+
+export const fetchWeatherData = (query) => {
+  const params = {
+    q: query,
+    APPID: appId,
+  };
+
+  const queryString = Object.keys(params)
+    .map((key) => `${key}=${params[key]}`)
+    .join('&');
+
+  const url = `${endpoint}?${queryString}`;
+
+  return fetch(url)
+    .then((response) => response.json());
+};
